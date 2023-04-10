52842024147e5ebd0a5390d1643b13625f710bc8
Oraan Levi <oraanlevi1@gmail.com>
2023-04-10 11:57:14 -0700

    saving backend files

---
+++ backend/data.js
@@ -0,0 +1,61 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Face Masks',
+      slug: 'face-mask',
+      category: 'FaceMask',
+      image: 'images/p1.jpg',
+      price: 120,
+      countInStock: 0,
+      brand: 'Formula10.0.6',
+      rating: 4.8,
+      numReviews: 30,
+      description:
+        'a very sufficient skin care product that leaves your face poreless',
+    },
+    {
+      _id: '2',
+      name: 'Strawberry Perfume',
+      slug: 'straw-perfume',
+      category: 'Straw',
+      image: 'images/p2.jpg',
+      price: 200,
+      countInStock: 5,
+      brand: 'Malin&Goetz',
+      rating: 5.2,
+      numReviews: 400,
+      description:
+        'a very very wild, sweet, and feminine take on what it means to always get a compliment',
+    },
+    {
+      _id: '3',
+      name: 'Caffiene Drops ',
+      slug: 'caffeine-drops',
+      category: 'CaffieneDrops',
+      image: 'images/p3.jpg',
+      price: 18,
+      countInStock: 60,
+      brand: 'TheOrdinary',
+      rating: 4.6,
+      numReviews: 209,
+      description:
+        'a twist on black spot treatment and a favorite of the hydration in under your eyes',
+    },
+    {
+      _id: '4',
+      name: 'Black Honey',
+      slug: 'black-honey-lips',
+      category: 'LipStick',
+      image: 'images/p4.jpg',
+      price: 30,
+      countInStock: 25,
+      brand: 'Claudile',
+      rating: 5.0,
+      numReviews: 453,
+      description: 'a natural and flawless color of bitten cherry lips',
+    },
+  ],
+};
+
+export default data;
